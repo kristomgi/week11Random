69c8c1bd416ad0e016e71b7fb6f2ea7765f56d0a
kristomgi <144623041+kristomgi@users.noreply.github.com>
2025-11-30 21:14:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/randomRecipe.routes');
 const app = express();
 
-app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
+app.use(express.json());
 app.use('/ingredients', ingredientRouter);
 app.use('/recipes', recipeRouter);
 app.use('/fullRecipes', fullRecipesRouter);
